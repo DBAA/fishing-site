0ed93d2dcba1f6b5b1643921039a6bf79e241f2d
Dustin Long <me@dustmop.io>
2023-07-19 18:48:55 -0400

    Add fish submit path, add comments explaining the routing

--- functions/index.js
+++ functions/index.js
@@ -6,12 +6,17 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+// middleware to handle routing between express & react
 app.use((req, res, next) => {
     if (/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)) {
+        // static assets handled by express
         return next();
     } else if (req.path.startsWith('/api')) {
+        // explicitly allow the backend api call (see handler below)
         return next();
     } else {
+        // otherwise serve the prebuilt react app bundle
+        // this lets react do client-side routing
         res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
         res.header('Expires', '-1');
         res.header('Pragma', 'no-cache');
@@ -19,8 +24,24 @@ app.use((req, res, next) => {
     }
 });
 
+// a backend endpoint
 app.get('/api', (req, res) => {
     res.send('{"message":"Success!"}')
 });
+
+app.get('/fish_submit.php', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+  
+    // heartbeat
+    if (req.query && req.query.status) {
+      res.send('online');
+      return;
+    }
+  
+    // print out the submitted fish object
+    // TODO: save this to firebase DB
+    console.log(JSON.stringify(req.query, null, ' '));
+    res.send('Success!');
+  });
 
 exports.app = functions.https.onRequest(app);
